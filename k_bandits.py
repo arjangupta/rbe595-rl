5749cc553936b6cb1c67c65070036848f755e6c8
Arjan Gupta <arjan.gupta@lindsay.com>
2023-09-07 11:54:05 -0500

    Try to add plot for 10-armed bandit distributions

--- k_bandits.py
+++ k_bandits.py
@@ -2,6 +2,7 @@
 # Write a program to generate figure 2.2 of the textbook
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class KArmedBandit:
     def __init__(self, num_arms=10):
@@ -9,6 +10,7 @@ class KArmedBandit:
         self.create_distributions()
         self.shift_distributions()
         self.print_distributions()
+        self.plot_distributions()
 
     def create_distributions(self, size=1000):
         """Create k (default 10) stationary probability distributions (default size 1000)
@@ -19,6 +21,7 @@ class KArmedBandit:
 
     def print_distributions(self):
         """Show means and variances of each bandit"""
+        print("---- True values of each action ----")
         for i in range(self.num_arms):
             mean = np.mean(self.distributions[i])
             mean_str = ""
@@ -40,6 +43,12 @@ class KArmedBandit:
         textbook_shifts = [0.20, -0.80, 1.50, 0.40, 1.05, -1.50, -0.15, -1.00, 1.75, -0.50]
         for i in range(self.num_arms):
             self.distributions[i] += textbook_shifts[i]
+
+    def plot_distributions(self):
+        """Plot the distributions of each bandit"""
+        for i in range(self.num_arms):
+            plt.hist(self.distributions[i], bins=100)
+            plt.show()
 
 if __name__ == "__main__":
     # Create a 10-armed bandit
