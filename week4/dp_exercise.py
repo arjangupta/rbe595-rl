82c7dec2ce88a03b723d966fe8e083c5a928d6d9
Arjan Gupta <arjan.gupta@lindsay.com>
2023-09-19 22:49:02 -0500

    Color the goal state red

--- week4/dp_exercise.py
+++ week4/dp_exercise.py
@@ -6,14 +6,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_2d_array(array):
+def plot_2d_array(array, goal_y=7, goal_x=10):
     """Takes in a 2D array of 0's and 1's and converts
     it to a plot of occupied and unoccupied spaces"""
 
     # Creating a figure and axes
     fig, ax = plt.subplots()
+    # Set the size of the figure
+    fig.set_size_inches(10, 20)
     # Creating a plot of the array
     ax.imshow(array, cmap = 'binary')
+    # Color the goal state red
+    ax.plot(goal_x, goal_y, 'ro')
     # Displaying the plot
     plt.show()
 
