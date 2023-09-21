23efb887bc99195c040569dcd7dadf24eca932ef
Arjan Gupta <arjan.gupta@lindsay.com>
2023-09-21 16:39:33 -0500

    Create the Policy Iteration class and write member functions for each step

--- week4/dp_exercise.py
+++ week4/dp_exercise.py
@@ -6,6 +6,86 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+class PolicyIteration:
+    """Class for the Policy Iteration algorithm as described in the Barto & Sutton textbook"""
+
+    def __init__(self, policy, grid_world, goal_x=10, goal_y=7, gamma=0.95, theta=0.01):
+        """Constructor for the Policy Iteration algorithm"""
+        self.policy = policy
+        self.grid_world = grid_world
+        self.goal_x = goal_x
+        self.goal_y = goal_y
+        self.gamma = gamma
+        self.theta = theta
+
+    def policy_evaluation(self):
+        """Performs policy evaluation on a policy"""
+        # Initialize a value function of zeros
+        value_function = np.zeros(self.grid_world.shape)
+        # Repeat until delta < theta
+        while True:
+            # Initialize delta to 0
+            delta = 0
+            # For each state in the grid world
+            for i in range(self.grid_world.shape[0]):
+                for j in range(self.grid_world.shape[1]):
+                    # If the state is unoccupied
+                    if self.grid_world[i, j] == 0:
+                        # Calculate the value function for the state
+                        v = self.calculate_value_function(i, j)
+                        # Calculate the difference between the old value function and the new value function
+                        delta = max(delta, abs(value_function[i, j] - v))
+                        # Update the value function
+                        value_function[i, j] = v
+            # If delta < theta, then break
+            if delta < self.theta:
+                break
+        # Return the value function
+        return value_function
+
+    def policy_improvement(self, value_function):
+        """Performs policy improvement on a policy"""
+        # Initialize a boolean flag to false
+        policy_stable = False
+        # For each state in the grid world
+        for i in range(self.grid_world.shape[0]):
+            for j in range(self.grid_world.shape[1]):
+                # If the state is unoccupied
+                if self.grid_world[i, j] == 0:
+                    # Store the old action
+                    old_action = self.policy[i, j]
+                    # Calculate the new action
+                    new_action = self.calculate_new_action(i, j, value_function)
+                    # Update the policy
+                    self.policy[i, j] = new_action
+                    # If the old action and the new action are the same, set the flag to true
+                    if old_action == new_action:
+                        policy_stable = True
+        # Return the policy and the boolean flag
+        return self.policy, policy_stable
+
+    def calculate_value_function(self, i, j):
+        """Calculates the value function for a state"""
+        # Calculate the value function for the state
+        v = 0
+        # If the state is not the goal state
+        if i != self.goal_y or j != self.goal_x:
+            # Calculate the value function for the state
+            v = np.cos(self.policy[i, j]) + self.gamma * self.calculate_value_function_for_action(i, j, self.policy[i, j])
+        # Return the value function
+        return v
+
+    def calculate_new_action(self, i, j, value_function):
+        """Calculates the new action for a state"""
+        # Initialize an array to store the value function for each action
+        value_function_for_action = np.zeros(4)
+        # For each action
+        for action in range(4):
+            # Calculate the value function for the action
+            value_function_for_action[action] = self.calculate_value_function_for_action(i, j, action)
+        # Return the action with the maximum value function
+        return np.argmax(value_function_for_action)
+
 def plot_2d_array_with_arrows(array, policy, goal_y=7, goal_x=10):
     """Takes in a 2D array of 0's and 1's and converts
     it to a plot of occupied and unoccupied spaces, with arrows"""
