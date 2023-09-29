94ff95a5a7151dc291bc06c04d05223f36b326f0
Arjan Gupta <arjangupta95@gmail.com>
2023-09-28 20:52:45 -0500

    Add improved printouts and progress bar

--- week5/monte_carlo.py
+++ week5/monte_carlo.py
@@ -13,6 +13,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import trange
 
 class EpisodeGenerator:
     """Generates episodes for the Monte Carlo algorithms,
@@ -87,25 +88,41 @@ class MonteCarloES:
     def __init__(self, episode_length=1000, num_episodes=1000, gamma=0.95):
         self.num_states = 6
         self.num_actions = 2
+
         # Randomly initialize policy for back and forward actions (0 and 1)
         self.policy = np.random.randint(self.num_actions, size=self.num_states)
+
         # Initialize Q(s,a) arbitrarily to real numbers
         self.Q = np.random.rand(self.num_states, self.num_actions)
+
         # Initialize returns to shape of Q, with empty lists
         self.returns = np.empty_like(self.Q, dtype=list)
         for i in range(self.returns.shape[0]):
             for j in range(self.returns.shape[1]):
                 self.returns[i, j] = []
+
         # Initialize episode length, number of episodes, and discount factor
         self.episode_length = episode_length
         self.num_episodes = num_episodes
         self.gamma = gamma
+
         # Initialize episode generator
         self.episode_generator = EpisodeGenerator(self.policy, self.episode_length, self.num_states, self.num_actions)
-    
+
+    def show_pi_q(self, show=True):
+        """Sets flag to show the policy and Q values"""
+        self.show_pi_q = show
+
+
     def run(self):
         """Runs the Monte Carlo algorithm for the specified number of episodes"""
-        for i in range(self.num_episodes):
+        if self.show_pi_q:
+            print("Initial policy:")
+            print(self.policy)
+            print("Initial Q values:")
+            print(self.Q)
+        print("Running Monte Carlo ES algorithm...")
+        for i in trange(self.num_episodes):
             # Generate an episode using the current policy
             episode = self.episode_generator.generate()
             G = 0
@@ -115,12 +132,14 @@ class MonteCarloES:
                 state, action, reward = step
                 # Update the return
                 G = self.gamma * G + reward
+
                 # Iterate through all previous steps in the episode, and
                 # check if the pair (state, action) appears in any previous step
                 pair_occurred = False
                 for j in range(len(episode) - i - 1):
                     if episode[j][0] == state and episode[j][1] == action:
                         pair_occurred = True
+
                 # "Unless the pair (state, action) appears in any previous step"
                 if not pair_occurred:
                     # Add the return to the returns
@@ -129,13 +148,14 @@ class MonteCarloES:
                     self.Q[state, action] = np.mean(self.returns[state, action])
                     # Update the policy
                     self.policy[state] = np.argmax(self.Q[state, :])
-                
-
+        print("Finished running Monte Carlo ES algorithm")
+        if self.show_pi_q:
+            print("Final policy:")
+            print(self.policy)
+            print("Final Q values:")
+            print(self.Q)
 
 if __name__ == "__main__":
     mc_es = MonteCarloES()
-    print(mc_es.Q)
-    print(mc_es.policy)
+    mc_es.show_pi_q(True)
     mc_es.run()
-    print(mc_es.Q)
-    print(mc_es.policy)
