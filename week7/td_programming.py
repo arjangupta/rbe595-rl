8f9c79389722288138eb128b1df022671106258c
Arjan Gupta <arjan.gupta@lindsay.com>
2023-10-12 16:27:15 -0500

    Plot the gridworld

--- week7/td_programming.py
+++ week7/td_programming.py
@@ -12,3 +12,50 @@
 # off and be sent back to the start. The agent receives a reward of -1 for each step it takes
 # that does not result in falling off the cliff. The agent receives a reward of -100 for falling
 # off the cliff. The agent receives a reward of 0 for reaching the goal state.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_gridworld():
+    """
+    Generates a gridworld for the cliff-walking problem.
+
+    Returns:
+        gridworld (list): A 4x12 grid of states, represented as a list of lists.
+    """
+    gridworld = []
+    for i in range(4):
+        row = []
+        for j in range(12):
+            row.append((i, j))
+        gridworld.append(row)
+    return gridworld
+
+def plot_gridworld(gridworld):
+    """
+    Plots a gridworld for the cliff-walking problem.
+
+    Args:
+        gridworld (list): A 4x12 grid of states, represented as a list of lists.
+    """
+    plt.figure()
+    plt.title("Cliff-Walking Gridworld")
+    plt.xlabel("Column")
+    plt.ylabel("Row")
+    plt.xlim(0, 12)
+    plt.ylim(0, 4)
+    plt.xticks(np.arange(0, 12, 1))
+    plt.yticks(np.arange(0, 4, 1))
+    plt.grid(True)
+    for i in range(4):
+        for j in range(12):
+            plt.text(j + 0.5, i + 0.5, str(gridworld[i][j]), ha="center", va="center")
+    plt.show()
+
+def main():
+    print("TD Programming Assignment")
+    gridworld = generate_gridworld()
+    plot_gridworld(gridworld)
+
+if __name__ == "__main__":
+    main()
