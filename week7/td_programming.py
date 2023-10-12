21b403b92df09e39462c25412bb1ad7974e0fe48
Arjan Gupta <arjan.gupta@lindsay.com>
2023-10-12 16:43:31 -0500

    Plot two example paths

--- week7/td_programming.py
+++ week7/td_programming.py
@@ -50,14 +50,10 @@ def plot_gridworld(gridworld, path1, path2):
     for j in range(12):
         if j != 0 and j != 11:
             plt.gca().add_patch(plt.Rectangle((j, 0), 1, 1, facecolor="grey"))
-    # Plot the path
-    for i in range(len(path1) - 1):
-        plt.plot([path1[i][1] + 0.5, path1[i + 1][1] + 0.5], [path1[i][0] + 0.5, path1[i + 1][0] + 0.5], color="blue")
-    for i in range(len(path2) - 1):
-        plt.plot([path2[i][1] + 0.5, path2[i + 1][1] + 0.5], [path2[i][0] + 0.5, path2[i + 1][0] + 0.5], color="red")
-    # Show legend
-    plt.plot([], [], color="blue", label="SARSA")
-    plt.plot([], [], color="red", label="Q-learning")
+    # Plot the paths and show the legend
+    plt.plot([x for x, _ in path1], [y for _, y in path1], "b-", label="Path 1")
+    plt.plot([x for x, _ in path2], [y for _, y in path2], "r-", label="Path 2")
+    plt.legend()
     # Put a big S at the start state
     plt.text(0.5, 0.5, "S", ha="center", va="center", fontsize=20)
     # Put a big G at the goal state
@@ -67,11 +63,9 @@ def plot_gridworld(gridworld, path1, path2):
 def main():
     print("TD Programming Assignment")
 
-    # Generate 2 example paths, taking different routes to the goal
-    path1 = [(0, 0), (0, 1), (0, 2), (0, 3), (1, 3), (2, 3), (3, 3), (3, 4), (3, 5), (3, 6),
-             (3, 7), (3, 8), (3, 9), (3, 10), (3, 11)]
-    path2 = [(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6),
-             (3, 7), (3, 8), (3, 9), (3, 10), (3, 11)]
+    # Generate 2 example paths, starting at (0.5, 0.5) and ending at (11.5, 0.5), make them take different routes
+    path1 = [(0.5, 0.5), (1.5, 0.5), (2.5, 0.5), (3.5, 0.5), (3.5, 1.5), (3.5, 2.5), (3.5, 3.5), (2.5, 3.5), (1.5, 3.5), (0.5, 3.5), (0.5, 2.5), (0.5, 1.5), (0.5, 0.5)]
+    path2 = [(0.5, 0.5), (1.5, 0.5), (2.5, 0.5), (3.5, 0.5), (3.5, 1.5), (3.5, 2.5), (3.5, 3.5), (2.5, 3.5), (1.5, 3.5), (0.5, 3.5), (0.5, 2.5), (1.5, 2.5), (2.5, 2.5), (3.5, 2.5), (3.5, 1.5), (3.5, 0.5), (2.5, 0.5), (1.5, 0.5), (0.5, 0.5)]
 
     gridworld = generate_gridworld()
     plot_gridworld(gridworld, path1, path2)
