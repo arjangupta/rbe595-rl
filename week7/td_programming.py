989b15f609124db7cbfae0982ff3f0f44ebb4d34
Arjan Gupta <arjangupta95@gmail.com>
2023-10-12 21:57:37 -0500

    Plot the plot with gridlines at 0.5

--- week7/td_programming.py
+++ week7/td_programming.py
@@ -128,29 +128,28 @@ class QLearningAgent:
 def plot_gridworld(path1, path2):
     """
     Plots a gridworld for the cliff-walking problem.
-
-    Args:
-        gridworld (list): A 4x12 grid of states, represented as a list of lists.
     """
     plt.figure()
     plt.gcf().set_size_inches(X_DIM, Y_DIM)
     plt.title("Cliff-Walking Gridworld")
+    # Set limits
     plt.xlim(0, X_DIM)
     plt.ylim(0, Y_DIM)
-    plt.xticks(np.arange(0, X_DIM, 1))
-    plt.yticks(np.arange(0, Y_DIM, 1))
+    # Show ticks at -0.5, 0.5, 1.5, etc.
+    plt.xticks(np.arange(-0.5, X_DIM, 1))
+    plt.yticks(np.arange(-0.5, Y_DIM, 1))
+    # Draw gridlines
     plt.grid(True)
-    for j in range(12):
-        if j != 0 and j != 11:
-            plt.gca().add_patch(plt.Rectangle((j, 0), 1, 1, facecolor="grey"))
+    # Draw the cliff as between x=0.5 and x=10.5, y=-0.5 and y=0.5
+    plt.fill_between([0.5, 10.5], [-0.5, -0.5], [0.5, 0.5], color="gray")
     # Plot the paths and show the legend
     plt.plot([x for x, _ in path1], [y for _, y in path1], "r-", label="Q-learning Path")
     plt.plot([x for x, _ in path2], [y for _, y in path2], "b-", label="Path 2")
     plt.legend()
-    # Put a big S at the start state
-    plt.text(0.5, 0.5, "S", ha="center", va="center", fontsize=20)
-    # Put a big G at the goal state
-    plt.text(11.5, 0.5, "G", ha="center", va="center", fontsize=20)
+    # Put a big S at the start state (0, 0)
+    plt.text(0, 0, "S", ha="center", va="center", fontsize=20)
+    # Put a big G at the goal state (11, 0)
+    plt.text(11, 0, "G", ha="center", va="center", fontsize=20)
     plt.show()
 
 def main():
