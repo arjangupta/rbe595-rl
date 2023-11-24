fa58f199b5ee62b2feb28b57c4f89a2d61952192
Arjan Gupta <arjangupta95@gmail.com>
2023-11-23 21:00:49 -0600

    Add tutorial on arithmetic operations for tensors

--- week12/pytorch-beginner-basics/tensors.py
+++ week12/pytorch-beginner-basics/tensors.py
@@ -27,7 +27,7 @@ print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor} \n")
 
 # Tensor attributes
-tensor = torch.rand(3, 4)
+tensor = torch.rand(10, 4)
 print(f"Shape of tensor: {tensor.shape}")
 print(f"Datatype of tensor: {tensor.dtype}")
 print(f"Device tensor is stored on: {tensor.device} \n")
@@ -47,3 +47,35 @@ print(f"Device tensor is stored on: {tensor.device} \n")
 print('First row: ', tensor[0])
 print('First column: ', tensor[:, 0])
 print('Last column: ', tensor[..., -1])
+
+# Joining tensors
+# torch.cat takes a list of tensors to concatenate together
+t1 = torch.cat([tensor, tensor], dim=1)
+print(f"Concatenate by columns: \n {t1} \n")
+
+# Arithmetic operations
+tensor2 = torch.from_numpy(np.array([[10.0, 15.0, 20.0], [30.0, 35.0, 40.0]]))
+print(f"Tensor 2: \n {tensor2} \n")
+if torch.cuda.is_available():
+    print("Moving tensor2 to GPU")
+    tensor2 = tensor2.to('cuda')
+# Matrix multiplication
+y1 = tensor2 @ tensor2.T
+y2 = tensor2.matmul(tensor2.T)
+y3 = torch.rand_like(y1)
+torch.matmul(tensor2, tensor2.T, out=y3)
+print("Matrix multiplication: \n")
+print(f"y1: \n {y1} \n")
+print(f"y2: \n {y2} \n")
+print(f"y3: \n {y3} \n")
+
+# Element-wise product
+z1 = tensor2 * tensor2
+z2 = tensor2.mul(tensor2)
+z3 = torch.rand_like(tensor2)
+torch.mul(tensor2, tensor2, out=z3)
+print("Element-wise product: \n")
+print(f"z1: \n {z1} \n")
+print(f"z2: \n {z2} \n")
+print(f"z3: \n {z3} \n")
+
