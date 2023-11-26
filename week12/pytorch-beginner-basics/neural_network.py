5efa971cf31454fa26cb72d77abe9057cd9b8cd5
Arjan Gupta <arjangupta95@gmail.com>
2023-11-25 18:27:44 -0600

    Walk thru compnents of the model

--- week12/pytorch-beginner-basics/neural_network.py
+++ week12/pytorch-beginner-basics/neural_network.py
@@ -38,3 +38,28 @@ logits = model(X)
 pred_probab = nn.Softmax(dim=1)(logits)
 y_pred = pred_probab.argmax(1)
 print(f"Predicted class: {y_pred}")
+
+# Let's break down the layers in the model
+
+# mini-batch of 3 images of size 28x28
+input_image = torch.rand(3,28,28)
+print(input_image.size())
+
+# nn.Flatten - converts each 2D 28x28 image into a 
+# contiguous array of 784 pixel values
+flatten = nn.Flatten()
+flat_image = flatten(input_image)
+print(flat_image.size())
+
+# nn.Linear - applies a linear transformation on the input
+# y = xA^T + b
+# A is the weight matrix, b is the bias vector
+# x is the input vector, y is the output vector
+layer1 = nn.Linear(in_features=28*28, out_features=20)
+hidden1 = layer1(flat_image)
+print(hidden1.size())
+
+# nn.ReLU - non-linear activation function
+print(f"Before ReLU: {hidden1}\n\n")
+hidden1 = nn.ReLU()(hidden1)
+print(f"After ReLU: {hidden1}")
