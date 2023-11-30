683f2e3d111e73300804294b584e523af9dfd51c
Arjan Gupta <arjangupta95@gmail.com>
2023-11-29 21:58:53 -0600

    Use .to on nn module to move it to GPU

--- week12/pytorch-beginner-basics/optimization.py
+++ week12/pytorch-beginner-basics/optimization.py
@@ -38,8 +38,15 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
-model = NeuralNetwork()
-model = model.to("cuda")
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+model = NeuralNetwork().to(device)
 
 # Hyperparameters
 learning_rate = 1e-3
@@ -53,7 +60,7 @@ def train_loop(dataloader, model, loss_fn, optimizer):
     # Unnecessary in this situation but added for best practices
     model.train()
     for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to("cuda"), y.to("cuda")
+        X, y = X.to(device), y.to(device)
         # Compute prediction and loss
         pred = model(X)
         loss = loss_fn(pred, y)
@@ -80,7 +87,7 @@ def test_loop(dataloader, model, loss_fn):
     # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
     with torch.no_grad():
         for X, y in dataloader:
-            X, y = X.to("cuda"), y.to("cuda")
+            X, y = X.to(device), y.to(device)
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
             correct += (pred.argmax(1) == y).type(torch.float).sum().item()
