bb05deed1d50590420ebbe51f2497accb6a5f9f3
Arjan Gupta <arjangupta95@gmail.com>
2023-12-08 12:03:15 -0600

    Add env file and move final project source code into src

--- final-project/final_project.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '../../../../workspace/aerial_gym_simulator')
-
-import isaacgym
-from aerial_gym.envs import *
-from aerial_gym.utils import get_args, task_registry
-
-env, env_cfg = task_registry.make_env("quad_with_obstacles")
