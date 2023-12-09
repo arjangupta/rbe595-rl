75548c317c17f71bfbb434ab406098b5f818ee16
Arjan Gupta <arjangupta95@gmail.com>
2023-12-09 13:00:05 -0600

    Define main()

--- final-project/src/main.py
+++ final-project/src/main.py
@@ -20,18 +20,22 @@ from aerial_gym.envs import *
 from aerial_gym.utils import get_args, task_registry
 import torch
 
-env, env_cfg = task_registry.make_env("quad_for_final_project")
+def main():
+    env, env_cfg = task_registry.make_env("quad_for_final_project")
 
-print("Number of environments", env_cfg.env.num_envs)
-command_actions = torch.zeros((env_cfg.env.num_envs, env_cfg.env.num_actions))
-command_actions[:, 0] = 0.0
-command_actions[:, 1] = 0.0
-command_actions[:, 2] = 0.0
-command_actions[:, 3] = 0.8
+    print("Number of environments", env_cfg.env.num_envs)
+    command_actions = torch.zeros((env_cfg.env.num_envs, env_cfg.env.num_actions))
+    command_actions[:, 0] = 0.0
+    command_actions[:, 1] = 0.0
+    command_actions[:, 2] = 0.0
+    command_actions[:, 3] = 0.8
 
-env.enable_onboard_cameras = True
+    env.enable_onboard_cameras = True
 
-for i in range(0, 5000):
-    obs, priviliged_obs, rewards, resets, extras = env.step(command_actions)
-        
-    print("Done", i)
+    for i in range(0, 5000):
+        obs, priviliged_obs, rewards, resets, extras = env.step(command_actions)
+            
+        print("Done", i)
+
+if __name__ == "__main__":
+    main()
