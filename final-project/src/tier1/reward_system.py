7f5be9dbb6418621ebc8dd36299b53fc783790a4
Arjan Gupta <arjangupta95@gmail.com>
2023-12-12 09:41:09 -0600

    Copy over reward system from paper

---
+++ final-project/src/tier1/reward_system.py
@@ -0,0 +1,60 @@
+"""
+This is the reward system for the quadcopter in the final project, as
+given in Camci et al. 2020.
+"""
+import numpy as np
+
+class RewardSystem:
+    def __init__(self):
+        # Initialize reward variables from paper
+        
+        # For time step distance calculation
+        self.dt_end = 0.0
+        self.dt_start = 0.0
+        self.delta_d = self.dt_end - self.dt_start
+
+        # Lower and upper limits on delta_d
+        self.delta_d_l = 0.0
+        self.delta_d_u = 1.0
+
+        # Maximum deviation distance
+        self.d_max = 5.0
+
+        # Lower and upper limits on reward
+        self.Rl = 0.0
+        self.Ru = 1.0
+
+        # Mild punishment for excessive deviation
+        self.R_dp = -0.5
+
+        # Drastic punishment for collision
+        self.R_cp = -1.0
+    
+    def f_delta_t(self, dt):
+        """This is the function that regulates the discount rate based on dmax"""
+        return 0.5 * ((np.tanh * ((2*self.d_max - dt)/self.d_max)) + 1)
+    
+    def determine_reward(self, did_collide):
+        """This function determines the reward for a given time step"""
+
+        # If collision, return drastic punishment
+        if did_collide:
+            return self.R_cp
+        
+        # If excessive deviation, mild punishment
+        if np.abs(self.dt_end) > self.d_max:
+            return self.R_dp
+        
+        # Calculate delta_d
+        self.delta_d = self.dt_end - self.dt_start
+        # Calculate reward as given in paper
+        if self.delta_d > self.delta_d_u:
+            return self.R_l * self.f_delta_t(self.dt_end)
+        elif self.delta_d < self.delta_d_l:
+            return self.R_u * self.f_delta_t(self.dt_end)
+        else:
+            return (self.R_l +
+                (self.R_u - self.R_l)*
+                ((self.delta_d_u - self.delta_d)/(self.delta_d_u - self.delta_d_l))
+            )*self.f_delta_t(self.dt_end)
+
