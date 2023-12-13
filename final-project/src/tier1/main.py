c48b808dbc15ad3ac0ef6ea103aca45522329d9b
Arjan Gupta <arjangupta95@gmail.com>
2023-12-13 09:30:59 -0600

    Declare gym interface

--- final-project/src/tier1/main.py
+++ final-project/src/tier1/main.py
@@ -6,34 +6,42 @@ from aeriel_robot_cfg_final_project import AerialRobotCfgFinalProjectTier1
 from aeriel_robot_final_project import AerialRobotFinalProjectTier1
 from action_primitives import QuadActionPrimitives
 
-def main():
-    task_registry.register( "quad_for_final_project", AerialRobotFinalProjectTier1, AerialRobotCfgFinalProjectTier1())
-    env, env_cfg = task_registry.make_env("quad_for_final_project")
+class GymInterface:
+    def __init__(self):
+        # Declare environment
+        task_registry.register( "quad_for_final_project", AerialRobotFinalProjectTier1, AerialRobotCfgFinalProjectTier1())
+        # Create environment
+        self.env, self.env_cfg = task_registry.make_env("quad_for_final_project")
+        # Declare command actions
+        self.command_actions = torch.zeros((self.env_cfg.env.num_envs, 3))
+        # Declare action primitives
+        self.action_primitives = QuadActionPrimitives()
 
-    command_actions = torch.zeros((env_cfg.env.num_envs, 3))
-
-    # Declare action primitives
-    action_primitives = QuadActionPrimitives()
-
-    for i in range(0, 5000):
+    def step(self, i):
         # Cycle through all actions
         action = i % 18
         print("action: " + str(action))
         # Get sampled curve
-        points = action_primitives.get_sampled_curve(action, num_samples=5)
+        points = self.action_primitives.get_sampled_curve(action, num_samples=5)
 
         # Step through all points
         for j in range(0, 5):
             # Set command actions
-            command_actions = torch.from_numpy(points[:,j])
+            self.command_actions = torch.from_numpy(points[:,j])
             if i % 250 == 0:
-                print(f"command_actions: {command_actions}")
+                print(f"command_actions: {self.command_actions}")
             for k in range(0, 50):
                 if j == 4 and k == 49:
-                    curr_pos = env.get_current_position()
+                    curr_pos = self.env.get_current_position()
                     print(f"Reached target pos of action: {curr_pos}")
                 # Step through the environment repeatedly
-                obs, priviliged_obs, rewards, resets, extras = env.step(command_actions)
+                obs, priviliged_obs, rewards, resets, extras = self.env.step(self.command_actions)
+
+def main():
+    gym_iface = GymInterface()
+
+    for i in range(0, 5000):
+        gym_iface.step(i)
 
 if __name__ == "__main__":
     main()
